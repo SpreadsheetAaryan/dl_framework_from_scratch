dd69aef71417bd85773d342f2711bdbf359fd67f
Aaryan Patel <119144330+SpreadsheetAaryan@users.noreply.github.com>
2025-07-05 09:03:50 -0700

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,56 @@
+from graphviz import Digraph
+
+def trace(root):
+  # builds a set of all nodes and edges in a graph
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes: # Corrected from 'not in not'
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot_scalar(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+
+    for n in nodes:
+        dot.node(name=str(id(n)), label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+
+def draw_dot_tensor(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir})
+
+    for n in nodes:
+        data_str = f"{n.data:.4f}" if n.data.ndim == 0 else f"{n.data.shape} array"
+        grad_str = f"{n.grad:.4f}" if n.grad.ndim == 0 else f"{n.grad.shape} array"
+
+        dot.node(name=str(id(n)), label = "{ %s | data %s | grad %s }" % (n.label, data_str, grad_str), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
