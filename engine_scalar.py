8f638e6b763d3963a354b9d06b5138e338902b93
Aaryan Patel <119144330+SpreadsheetAaryan@users.noreply.github.com>
2025-07-09 15:37:46 -0700

    update

--- engine_scalar.py
+++ engine_scalar.py
@@ -85,6 +85,14 @@ class Value:
     out._backward = _backward
     return out
 
+  def ln(self):
+    out = Value(math.log(self.data), (self, ), 'ln')
+
+    def _backward():
+      self.grad += (1 / self.data) * out.grad
+    out._backward = _backward
+    return out
+
   def backward(self):
     topo = []
     visited = set()
